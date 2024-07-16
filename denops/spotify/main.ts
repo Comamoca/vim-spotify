f761adfd196ea24c828da467ddd01023ed823c7b
Comamoca <comamoca.dev@gmail.com>
2024-07-16 17:26:23 +0900

    :sparkles: (denops/main.ts) Added auth future.

--- denops/spotify/main.ts
+++ denops/spotify/main.ts
@@ -1,37 +1,63 @@
-import { Denops, ensure, execute, is } from "../../deps.ts";
-import { SpotifyClient } from "https://deno.land/x/soundify@v1.1.0/mod.ts";
-import { nowplaying, pause, resume, toNext, toPrevious } from "./api.ts";
-import { type HTTPClient } from "https://deno.land/x/soundify@v1.1.0/client.ts";
+import { Denops, ensure, execute, is, isNullish } from "../../deps.ts";
+import { SpotifyClient } from "https://deno.land/x/soundify@v1.1.5/mod.ts";
+import {
+  nowplaying,
+  pause,
+  resume,
+  startAuth,
+  toNext,
+  toPrevious,
+} from "./api.ts";
+import { type HTTPClient } from "https://deno.land/x/soundify@v1.1.5/client.ts";
+import { loadPluginData, PluginDataType } from "../../load.ts";
 
 export async function main(denops: Denops): Promise<void> {
   console.log("Hello from vim-spotiy");
-  // Setup spotify client
-  const token = Deno.readTextFileSync("token");
-  const client: HTTPClient = new SpotifyClient(token);
+  // Load data of token.
+  const data = await loadPluginData();
 
-  denops.dispatcher = {
-    nowplaying(): Promise<unknown> {
-      return Promise.resolve(nowplaying(client));
-    },
-    pause() {
-      return Promise.resolve(pause(client));
-    },
-    resume() {
-      return Promise.resolve(resume(client));
-    },
-    playbackState() {
-      return Promise.resolve();
-    },
-    next() {
-      return Promise.resolve(toNext(client));
-    },
-    previous() {
-      return Promise.resolve(toPrevious(client));
-    },
-  };
+  if (is.Nullish(data)) {
+    // When `token.json` not found, launch server and get token.
+    console.log("認証処理が完了してません。 認証処理を完了させてください。");
 
-  await execute(
-    denops,
-    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
-  );
+    denops.dispatcher = {
+      auth() {
+        return Promise.resolve(startAuth());
+      },
+    };
+  } else {
+    // Expect token is aleady got.
+    const token = data.access_token;
+
+    // Setup spotify client
+    const client: HTTPClient = new SpotifyClient(token);
+
+    const spotifyAPIFunctions = {
+      nowplaying() {
+        return Promise.resolve(nowplaying(client));
+      },
+      pause() {
+        return Promise.resolve(pause(client));
+      },
+      resume() {
+        return Promise.resolve(resume(client));
+      },
+      playbackState() {
+        return Promise.resolve();
+      },
+      next() {
+        return Promise.resolve(toNext(client));
+      },
+      previous() {
+        return Promise.resolve(toPrevious(client));
+      },
+    };
+
+    denops.dispatcher = {
+      ...spotifyAPIFunctions,
+      auth() {
+        return Promise.resolve(startAuth());
+      },
+    };
+  }
 }
