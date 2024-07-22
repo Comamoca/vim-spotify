12064a25e41c8438f0d3ac13d93d522e677153d5
Comamoca <comamoca.dev@gmail.com>
2024-07-22 20:31:26 +0900

    :sparkles: (spotify/main.ts) Update resume function.
    Update function of `resume` for intuitive operation.

--- denops/spotify/main.ts
+++ denops/spotify/main.ts
@@ -1,5 +1,6 @@
 import { Denops, ensure, execute, is } from "../../deps.ts";
-import { SpotifyClient } from "https://deno.land/x/soundify@v1.1.5/mod.ts";
+// import { SpotifyClient } from "https://deno.land/x/soundify@v1.1.5/mod.ts";
+import { type StartResumePlaybackBody } from "https://deno.land/x/soundify@v1.1.5/endpoints/mod.ts";
 import { refreshToken } from "./refresh.ts";
 import {
   availableDevices,
@@ -12,80 +13,77 @@ import {
   toNext,
   toPrevious,
 } from "./api.ts";
-import { type HTTPClient } from "https://deno.land/x/soundify@v1.1.5/client.ts";
-import { loadPluginData, PluginDataType } from "./load.ts";
-
-async function useClient(
-  callback: (client: HTTPClient) => Promise<unknown | null | undefined>,
-) {
-  // Load data of token.
-  const data = await loadPluginData();
-
-  if (is.Nullish(data)) {
-    // When `token.json` not found, launch server and get token.
-    console.log(
-      "Authentication process has not been completed. Please call `spotify#auth()` to complete the authentication process.",
-    );
-  }
-
-  // Expect token is aleady got.
-  const token = PluginDataType.parse(data).access_token;
-
-  // Setup spotify client
-  const client: HTTPClient = new SpotifyClient(token);
-
-  return callback(client);
-}
+// import { getPlaybackState } from "https://deno.land/x/soundify@v1.1.5/endpoints/mod.ts";
+// import { type HTTPClient } from "https://deno.land/x/soundify@v1.1.5/client.ts";
+// import { loadPluginData, PluginDataType } from "./load.ts";
+import { createClient, useClient } from "./utils.ts";
 
 export async function main(denops: Denops): Promise<void> {
   console.log("Hello from vim-spotiy");
 
-  try {
-    denops.dispatcher = {
-      async playbackStart() {
-        return await Promise.resolve(useClient(playbackStart));
-      },
-      async play() {
-        return await Promise.resolve();
-      },
-      async nowplaying() {
-        return await Promise.resolve(useClient(nowplaying));
-      },
-      async pause() {
-        return await Promise.resolve(useClient(pause));
-      },
-      async resume() {
-        return await Promise.resolve(useClient(resume));
-      },
-      async playbackState() {
-        return await Promise.resolve(useClient(playbackState));
-      },
-      async next() {
-        return await Promise.resolve(useClient(toNext));
-      },
-      async previous() {
-        return await Promise.resolve(useClient(toPrevious));
-      },
-      async availableDevices() {
-        return await Promise.resolve(useClient(availableDevices));
-      },
-      async auth(sec: unknown) {
-        if (is.Number(sec)) {
-          return await Promise.resolve(startAuth(sec));
-        } else {
-          return await Promise.resolve(startAuth());
-        }
-      },
-    };
-  } catch (e) {
-    // Spotify API Error
-    if (e.body.error.status == 401 && e.name == "SpotifyError") {
-      try {
-        await refreshToken();
-        console.log("Refreshed token by automatically.");
-      } catch {
-        console.log("Can not refreshing token. Please retry authorize.");
+  denops.dispatcher = {
+    async playbackStart() {
+      return await Promise.resolve(useClient(playbackStart));
+    },
+    async play() {
+      return await Promise.resolve();
+      // (async () => {
+      //   while (true) {
+      //     await Promise.any([]);
+      //
+      //     const client = await createClient();
+      //     const state = await playbackState(client);
+      //     const isPlaying = is.Boolean(state?.is_playing);
+      //
+      //     console.log();
+      //
+      //     // When pause playback, stop polling nowplaying.
+      //     if (!isPlaying) {
+      //       break;
+      //     }
+      //   }
+      // })();
+    },
+    async nowplaying() {
+      return await Promise.resolve(useClient(nowplaying));
+    },
+    async pause() {
+      return await Promise.resolve(useClient(pause));
+    },
+    async resume(id: unknown, context: unknown, uris: unknown) {
+      console.log("id", id, "context", context, "uris", uris);
+      return await Promise.resolve(
+        useClient((client) =>
+          resume(
+            client,
+            id = is.String(id) ? id : undefined,
+            context = is.String(context) ? context : undefined,
+            uris = is.ArrayOf(is.String)(uris) ? uris : undefined,
+          )
+        ),
+      );
+    },
+    async playbackState() {
+      return await Promise.resolve(useClient(playbackState));
+    },
+    async next() {
+      return await Promise.resolve(useClient(toNext));
+    },
+    async previous() {
+      return await Promise.resolve(useClient(toPrevious));
+    },
+    async availableDevices() {
+      return await Promise.resolve(useClient(availableDevices));
+    },
+    async auth(sec: unknown) {
+      if (is.Number(sec)) {
+        return await Promise.resolve(startAuth(sec));
+      } else {
+        return await Promise.resolve(startAuth());
       }
-    }
-  }
+    },
+    async refresh() {
+      return await Promise.resolve(refreshToken());
+    },
+  };
 }
